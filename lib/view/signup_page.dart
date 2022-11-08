2d09e37db1c27e9d7dd082133bc38df93a8342c0
Gideon Okoroafor <okoro062@umn.edu>
2022-11-08 11:42:43 -0600

    added feedback for users on creating account and also added functions that stores user info in firestore

--- lib/view/signup_page.dart
+++ lib/view/signup_page.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: unused_field
+
+import 'package:charity_donations/model/charity_donations_model.dart';
+import 'package:charity_donations/utils/loading.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,20 +19,40 @@ class SignUpPage extends StatefulWidget {
 
 class _SignUpPageState extends State<SignUpPage> {
   // text controllers
+  final _firstnameController = TextEditingController();
+  final _lastnameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
+  final _addressController = TextEditingController();
+
+  // model reference
+  CharityDonationsModel model = CharityDonationsModel();
+
+  // adding a loading widget
+  bool _isLoading = false;
 
   // Sign up method
   Future signUp() async {
     try {
       if (_passwordController.text.trim() ==
           _confirmPasswordController.text.trim()) {
+        setState(() {
+          _isLoading = true;
+        }); // added a loading widget
         await FirebaseAuth.instance.createUserWithEmailAndPassword(
             email: _emailController.text.trim(),
             password: _passwordController.text.trim());
+        model.dbAddUserDetails(
+            _firstnameController.text.trim(),
+            _lastnameController.text.trim(),
+            _emailController.text.trim(),
+            _addressController.text.trim());
       }
     } on FirebaseAuthException catch (e) {
+      setState(() {
+        _isLoading = false; // added loading
+      });
       showDialog(
           context: context,
           builder: (context) {
@@ -42,7 +66,10 @@ class _SignUpPageState extends State<SignUpPage> {
   // dispose of controllers when not in use
   @override
   void dispose() {
+    _firstnameController.dispose();
+    _lastnameController.dispose();
     _emailController.dispose();
+    _addressController.dispose();
     _passwordController.dispose();
     _confirmPasswordController.dispose();
     super.dispose();
@@ -50,46 +77,70 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _backgroundImage(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Text(
-              'Create Account',
-              style: GoogleFonts.bebasNeue(
-                  fontSize: 45, color: Colors.black.withOpacity(0.8)),
-              // style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
+    return _isLoading
+        ? const Loading()
+        : Scaffold(
+            backgroundColor: Colors.grey[300],
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // _backgroundImage(),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Image(
+                    image: AssetImage('assets/amal_bluegrey_logo1.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                    child: Text(
+                      'Create Account',
+                      style: GoogleFonts.bebasNeue(
+                          fontSize: 45, color: Colors.blueGrey.shade400),
+                      // style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _inputFirstName(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _inputLastName(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _inputEmail(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _inputAddress(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _inputPassword(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _confirmPassword(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _signUpButton(context),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _returnToSignInPage(context),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          _inputEmail(),
-          const SizedBox(
-            height: 10,
-          ),
-          _inputPassword(),
-          const SizedBox(
-            height: 10,
-          ),
-          _confirmPassword(),
-          const SizedBox(
-            height: 10,
-          ),
-          _signUpButton(context),
-          const SizedBox(
-            height: 10,
-          ),
-          _returnToSignInPage(context)
-        ],
-      )),
-    );
+          );
   }
 
   // text field for the users email
@@ -113,29 +164,6 @@ class _SignUpPageState extends State<SignUpPage> {
           ),
           fillColor: Colors.grey[200],
           filled: true,
-        ),
-      ),
-    );
-  }
-
-  _backgroundImage() {
-    Size size = MediaQuery.of(context).size;
-    const double appPadding = 20.0;
-    return ClipPath(
-      clipper: CurveClipper(),
-      child: Container(
-        height: size.height * .53,
-        width: size.width,
-        color: Colors.blueGrey.withOpacity(0.8),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: appPadding / 2, vertical: appPadding * 3),
-          child: Center(
-            child: Image(
-              image: AssetImage('assets/amal_white_logo1.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
         ),
       ),
     );
@@ -223,35 +251,81 @@ class _SignUpPageState extends State<SignUpPage> {
       child: MyButton(
           label: 'Sign Up',
           onTap: signUp,
-          // () {
-          //   Navigator.of(context)
-          //       .push(MaterialPageRoute(builder: (BuildContext context) {
-          //     return const SignInAuth();
-          //   }));
-          // },
           height: 50,
           width: 400,
           color: Colors.blueGrey.withOpacity(0.8)),
     );
   }
 
-  // sign up as organization button
-  _signUpAsOrgButton(context) {
+  _inputFirstName() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: MyButton(
-          label: 'Sign Up As Organization',
-          onTap: signUp,
-          // () {
-          //   Navigator.of(context)
-          //       .push(MaterialPageRoute(builder: (BuildContext context) {
-          //     return const SignInAuth();
-          //   }));
-          // },
-          height: 50,
-          width: 400,
-          color: Colors.blueGrey.withOpacity(0.8)),
+      child: TextField(
+        controller: _firstnameController,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.white)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.blueGrey)),
+          hintText: 'Enter Firstname',
+          prefixIcon: const Icon(
+            Icons.person,
+            color: Colors.blueGrey,
+          ),
+          fillColor: Colors.grey[200],
+          filled: true,
+        ),
+      ),
     );
   }
 
+  _inputLastName() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: TextField(
+        controller: _lastnameController,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.white)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.blueGrey)),
+          hintText: 'Enter Lastname',
+          prefixIcon: const Icon(
+            Icons.person,
+            color: Colors.blueGrey,
+          ),
+          fillColor: Colors.grey[200],
+          filled: true,
+        ),
+      ),
+    );
+  }
+
+  _inputAddress() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: TextField(
+        controller: _addressController,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.white)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.blueGrey)),
+          hintText: 'Enter Address',
+          prefixIcon: const Icon(
+            Icons.home,
+            color: Colors.blueGrey,
+          ),
+          fillColor: Colors.grey[200],
+          filled: true,
+        ),
+      ),
+    );
+  }
 }
