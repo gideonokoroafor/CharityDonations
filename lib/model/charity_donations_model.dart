4c20a0434610d7a338a22d595bbcd8b3feafd7ec
Gideon Okoroafor <okoro062@umn.edu>
2022-11-08 11:39:14 -0600

    model

---
+++ lib/model/charity_donations_model.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class CharityDonationsModel {
+  final firestoreInstance = FirebaseFirestore.instance;
+  late final _uid;
+  final auth = FirebaseAuth.instance;
+  late User currentUser;
+  getCurrentUser() {
+    currentUser = auth.currentUser!;
+    if (auth.currentUser != null) {
+      // User is signed in
+      _uid = FirebaseAuth.instance.currentUser!.uid.toString();
+    }
+    return _uid;
+  }
+
+  // Add user details to the database and store their userID
+  Future dbAddUserDetails(
+      String firstname, String lastName, String email, String address) async {
+        await FirebaseFirestore.instance.collection('users').add({
+          'first name': firstname,
+          'last name': lastName,
+          'email': email,
+          'address': address,
+          'userId': getCurrentUser(),
+      });
+  }
+
+  // Add organizations details to the database and store their userID
+  Future dbAddOrgDetails(
+      String firstname, String lastName, String email, String address) async {
+        await FirebaseFirestore.instance.collection('users').add({
+          'first name': firstname,
+          'last name': lastName,
+          'email': email,
+          'address': address,
+          'userId': getCurrentUser(),
+      });
+  }
+
+}
