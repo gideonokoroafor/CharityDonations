815d64cffbb23cfb0a55be5562ff1b3bf0663d78
Gideon Okoroafor <okoro062@umn.edu>
2022-11-08 11:41:36 -0600

    home page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: unused_field
+
+import 'package:charity_donations/utils/loading.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,25 +13,34 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // final user = FirebaseAuth.instance.currentUser!;
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome!'),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.blueGrey,
-              child: const Text('Sign Out'),
-            )
-          ],
-        ),
-      ),
-    );
+    return _isLoading
+        ? const Loading()
+        : Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Welcome!'),
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      FirebaseAuth.instance.signOut();
+                      setState(() {
+                        _isLoading = false;
+                      });
+                    },
+                    color: Colors.blueGrey,
+                    child: const Text('Sign Out'),
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
