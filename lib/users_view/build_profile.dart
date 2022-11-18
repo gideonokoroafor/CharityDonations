9511d903e5654fb2e9dad4a60aa3f1eb62296e7c
Gideon Okoroafor <okoro062@umn.edu>
2022-11-18 15:22:38 -0600

    build profile for users

--- lib/users_view/build_profile.dart
+++ lib/users_view/build_profile.dart
@@ -1,6 +1,9 @@
 import 'package:charity_donations/users_view/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../utils/myButtons.dart';
 
@@ -12,6 +15,24 @@ class BuildProfile extends StatefulWidget {
 }
 
 class _BuildProfileState extends State<BuildProfile> {
+  final _addressController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _numberController = TextEditingController();
+
+  List<String> gender = ['Male', 'Female', 'Other'];
+
+  final firestoreInstance = FirebaseFirestore.instance;
+
+  // void pickUploadImage() async {
+  //   final img = await ImagePicker().pickImage(
+  //     source: ImageSource.gallery,
+  //     maxWidth: 512,
+  //     maxHeight: 512,
+  //     imageQuality: 75,
+  //   );
+  //   Reference ref = FirebaseStorage.instance.ref().child('assets/amal_bluegrey_logo1.png');
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,43 +43,138 @@ class _BuildProfileState extends State<BuildProfile> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Thank you for signing up!',
-                  style: GoogleFonts.bebasNeue(
-                      fontSize: 45, color: Colors.black.withOpacity(0.8)),
-                  textAlign: TextAlign.left,
+                const SizedBox(
+                  height: 20,
                 ),
-                Text(
-                  "Let's build your profile",
-                  style: GoogleFonts.bebasNeue(
-                      fontSize: 25, color: Colors.blueGrey.shade400),
-                  textAlign: TextAlign.left,
-                ),
-                _continueButton(context)
-            ]),
+                // Text(
+                //   'Thank you for signing up!',
+                //   style: GoogleFonts.bebasNeue(
+                //       fontSize: 45, color: Colors.black.withOpacity(0.8)),
+                //   textAlign: TextAlign.left,
+                // ),
+                // Text(
+                //   "Let's build your profile",
+                //   style: GoogleFonts.bebasNeue(
+                //       fontSize: 25, color: Colors.blueGrey.shade400),
+                //   textAlign: TextAlign.left,
+                // ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Text(
+                      //       'Thank you for signing up!',
+                      //       style: GoogleFonts.bebasNeue(
+                      //           fontSize: 45, color: Colors.black.withOpacity(0.8)),
+                      //       textAlign: TextAlign.left,
+                      // ),
+                      // Text(
+                      //       "Let's build your profile",
+                      //       style: GoogleFonts.bebasNeue(
+                      //           fontSize: 25, color: Colors.blueGrey.shade400),
+                      //       textAlign: TextAlign.left,
+                      // ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _imageProfile(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _inputPhoneNumber(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _inputDescription(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _continueButton(context)
+                    ]),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  _imageProfile() {
+    return Column(
+      children: [
+        const CircleAvatar(
+          radius: 84,
+          backgroundColor: Colors.blueGrey,
+          child: CircleAvatar(
+            radius: 80.0,
+            backgroundImage: AssetImage('assets/amal_logo1.png'),
+            backgroundColor: Colors.white,
+            // foregroundColor: ,
+          ),
+        ),
+        _changeProfilePicture()
+      ],
+    );
+  }
+
   _continueButton(context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Center(
-        child: MyButton(
-            label: 'Continue',
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (BuildContext context) {
-                return const HomePage();
-              }));
-            },
-            height: 50,
-            width: 200,
-            color: Colors.blueGrey.withOpacity(0.8)),
+    return Center(
+      child: MyButton(
+          label: 'Continue',
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context) {
+              return const HomePage();
+            }));
+          },
+          height: 50,
+          width: 200,
+          color: Colors.blueGrey.withOpacity(0.8)),
+    );
+  }
+
+  _inputUsername() {
+    return TextField(
+        controller: _addressController,
+        decoration: textDecoration('Enter address'));
+  }
+
+  _inputPhoneNumber() {
+    return TextField(
+        controller: _numberController,
+        decoration: textDecoration('Enter mobile number: 444-444-4444'));
+  }
+
+  _inputDescription() {
+    return TextField(
+        controller: _descriptionController,
+        maxLines: 4,
+        decoration: textDecoration('Write about yourself'));
+  }
+
+  
+
+  _changeProfilePicture() {
+    return Center(
+      child: TextButton(
+        child: const Text(
+          "Change profile picture",
+        ),
+        onPressed: () {},
       ),
     );
   }
 
+  InputDecoration textDecoration(String hint) {
+    return InputDecoration(
+      enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.white)),
+      focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.blueGrey)),
+      hintText: hint,
+      fillColor: Colors.grey[200],
+      filled: true,
+    );
+  }
 }
