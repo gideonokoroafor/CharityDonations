c13b8aeccd8a3c1662edb7f80cb4b398a82f9ce0
Gideon Okoroafor <okoro062@umn.edu>
2022-11-23 15:12:14 -0600

    created search page

---
+++ lib/users_view/search_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Text('Categories'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: const Center(
+          child: Text(
+        'Search Page',
+        style: TextStyle(fontSize: 40),
+      )),
+    );
+  }
+}
