a936e27c53f31769dc1f133c8cfd70cda96c9d57
Gideon Okoroafor <okoro062@umn.edu>
2022-11-30 23:29:26 -0600

    tmp user profile

--- lib/users_view/tmp_user_profile.dart
+++ lib/users_view/tmp_user_profile.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class TmpUserProfile extends StatefulWidget {
@@ -8,18 +10,62 @@ class TmpUserProfile extends StatefulWidget {
 }
 
 class _TmpUserProfileState extends State<TmpUserProfile> {
+  User? user = FirebaseAuth.instance.currentUser!;
+  String _firstname = "";
+  String _lastname = "";
+  String _fullname = "";
+  String _email = "";
+  String _uid = "";
+
+  void getData() async {
+    _uid = user!.uid;
+    final DocumentSnapshot dbReference =
+        await FirebaseFirestore.instance.collection('users').doc(_uid).get();
+    // if (mounted) {
+    setState(() {
+      _firstname = dbReference.get('firstname');
+      _lastname = dbReference.get('lastname');
+      _fullname = "$_firstname $_lastname";
+      _email = dbReference.get('email');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: const Text('User Profile'),
         centerTitle: true,
+        backgroundColor: Colors.blueGrey,
       ),
-      body: const Center(
-          child: Text(
-        'User Profile Page',
-        style: TextStyle(fontSize: 40),
-      )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome! $_fullname'),
+            Text(_email),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.blueGrey,
+              child: const Text('Sign Out'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
