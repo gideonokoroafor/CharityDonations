c50fb15c9535a9bbd56cd1212bbb706e3560e86f
Gideon Okoroafor <okoro062@umn.edu>
2022-11-30 23:29:02 -0600

    search page is now populated with different categories

--- lib/users_view/search_page.dart
+++ lib/users_view/search_page.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: library_prefixes
+
+import 'package:charity_donations/utils/constants.dart' as Constants;
 import 'package:flutter/material.dart';
+
+import '../utils/image_button.dart';
+import 'category_pages.dart/bags_and_shoes.dart';
+import 'category_pages.dart/clothings.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,20 +15,200 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Categories'),
+        title: const Text('Search'),
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
-      body: const Center(
-          child: Text(
-        'Search Page',
-        style: TextStyle(fontSize: 40),
-      )),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _clothingAndShoesRow(context),
+            _kitchenAndFurnitureRow(context),
+            _sportsAndToysRow(context),
+            _booksAndElectronicsRow(context)
+          ],
+        ),
+      ),
     );
   }
+
+  _clothingAndShoesRow(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: Row(
+        children: [
+          Flexible(
+            child: ImageButton(
+              label: 'Clothings', 
+              onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const Clothings(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ), 
+              image: Constants.clothing_images
+            ),
+          ),
+    
+          Flexible(
+            child: ImageButton(
+              label: 'Bags and Shoes', 
+              onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const BagsAndShoes(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ), 
+              image: Constants.bags_shoes_image
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _kitchenAndFurnitureRow(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: Row(
+        children: [
+          Flexible(
+            child: ImageButton(
+                label: 'Kitchen Appliances',
+                onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const Clothings(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ),
+                image: Constants.kitchen_image
+              ),
+          ),
+          Flexible(
+            child: ImageButton(
+                label: 'Furnitures',
+                onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const BagsAndShoes(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ),
+                image: Constants.furniture_image
+              ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _sportsAndToysRow(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: Row(
+        children: [
+          Flexible(
+            child: ImageButton(
+                label: 'Sporting Goods',
+                onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const Clothings(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ),
+                image: Constants.sporting_image
+              ),
+          ),
+          Flexible(
+            child: ImageButton(
+                label: 'Toys',
+                onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const BagsAndShoes(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ),
+                image: Constants.toy_image
+              ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _booksAndElectronicsRow(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: Row(
+        children: [
+          Flexible(
+            child: ImageButton(
+                label: 'Books',
+                onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const Clothings(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ),
+                image: Constants.book_image
+              ),
+          ),
+          Flexible(
+            child: ImageButton(
+                label: 'Electronics',
+                onTap: () => Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const BagsAndShoes(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    ),
+                image: Constants.electronics_image
+              ),
+          )
+        ],
+      ),
+    );
+  }
+
+
 }
