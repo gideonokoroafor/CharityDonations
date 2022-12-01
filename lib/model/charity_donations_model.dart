4fdc7481d33946adf7d780f58541c3920cf1374a
Gideon Okoroafor <okoro062@umn.edu>
2022-11-30 23:26:46 -0600

    model

--- lib/model/charity_donations_model.dart
+++ lib/model/charity_donations_model.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
-import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-import 'package:path/path.dart' as Path;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:uuid/uuid.dart';
 
 class CharityDonationsModel {
@@ -16,6 +13,8 @@ class CharityDonationsModel {
   late User currentUser;
   late firebase_storage.Reference ref;
   late CollectionReference imgRef;
+  var uuid = const Uuid();
+
   getCurrentUser() {
     currentUser = auth.currentUser!;
     if (auth.currentUser != null) {
@@ -38,36 +37,38 @@ class CharityDonationsModel {
 
   // Add organizations details to the database and store their userID
   Future dbAddOrgDetails(String orgName, String email, String docId) async {
-    await FirebaseFirestore.instance.collection('users').doc(docId).set({
+    await FirebaseFirestore.instance
+        .collection('organizations')
+        .doc(docId)
+        .set({
       'orgName': orgName,
       'email': email,
       'userId': getCurrentUser(),
     });
   }
 
-  Future dbUploadImgWithDetails(
-    File img,
-    String docId,
-    String category,
-    String fullName,
-    String itemName,
-    String itemDescription,
-  ) async {
-    ref = firebase_storage.FirebaseStorage.instance
-        .ref()
-        .child('$category/${Path.basename(img.path)}')
-        .child('$category/${Path.basename(img.path)}');
-    await ref.putFile(img).whenComplete(() async {
-      await ref.getDownloadURL().then((value) {
-        FirebaseFirestore.instance.collection(category).doc(DateTime.now().toString()).set({
-          'fullname': fullName,
-          'itemName': itemName,
-          'category': category,
-          'description': itemDescription,
-          'url': value,
-          'userId': docId
-        });
-      });
+  Future dbDonationDetails(
+      String fullname,
+      String itemName,
+      String docId,
+      String category,
+      String itemDescription,
+      List<String> url,
+      String uuid) async {
+    await FirebaseFirestore.instance.collection('donations').doc(uuid).set({
+      'fullname': fullname,
+      'itemName': itemName,
+      'category': category,
+      'description': itemDescription,
+      'url': url,
+      'userId': docId,
+      'datePublished': DateTime.now()
     });
   }
+
+  // Future dbAddUserProfile(
+  //   String pic, String phoneNumber, String aboutMe, String gender, String proffession) async {
+  //     await FirebaseFirestore.instance.collection('users').add('data':pic,);
+  //   }
+
 }
