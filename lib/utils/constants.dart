35b6affbeb5293f674a10a36a9ea570f7878a3a4
Gideon Okoroafor <okoro062@umn.edu>
2022-12-05 09:51:09 -0600

    constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,8 @@
 // Categeories lists
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:flutter/material.dart';
+
 List<String> items = [
   'CLOTHING',
   'BAGS AND SHOES',
@@ -30,3 +32,69 @@ String get book_image =>
     'https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop';
 String get electronics_image =>
     'https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2022/07/electronic-devices.jpg';
+String get default_profile_pic => 
+    'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E';
+
+
+myButton(String text, IconData icon, Function() ontap) {
+  return GestureDetector(
+    onTap: ontap,
+    child: Container(
+      width: 450,
+      height: 60,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+            color: Colors.blueGrey,
+            strokeAlign: BorderSide.strokeAlignCenter,
+            style: BorderStyle.solid,
+            width: 2),
+        color: Colors.transparent,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.blueGrey,
+                  size: 25,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  text,
+                  style: const TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const Icon(
+              Icons.navigate_next,
+              color: Colors.blueGrey,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+myButton1(String text) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Text(
+      text,
+      maxLines: 4,
+      style: const TextStyle(
+          color: Colors.blueGrey,
+          fontSize: 18,
+          fontWeight: FontWeight.normal),
+    ),
+  );
+}
