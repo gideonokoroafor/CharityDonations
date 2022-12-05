610544fc9f073a24bb34cf01319b1f8dffb321fc
Gideon Okoroafor <okoro062@umn.edu>
2022-12-05 09:42:14 -0600

    landing page from the signup/login. just contains the buttom navigation bar

--- lib/utils/landing_page.dart
+++ lib/utils/landing_page.dart
@@ -2,7 +2,7 @@ import 'package:charity_donations/users_view/chat_page.dart';
 import 'package:charity_donations/users_view/home_page.dart';
 import 'package:charity_donations/users_view/make_donations.dart';
 import 'package:charity_donations/users_view/search_page.dart';
-import 'package:charity_donations/users_view/tmp_user_profile.dart';
+import 'package:charity_donations/users_view/user_profile.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
